a11a42dd6adb712ccef393fcc92bff549134ed03
shepherd <pucca1988z@gmail.com>
2023-03-15 13:15:35 +0800

    update wording

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -216,16 +216,16 @@ export default function Header() {
           
         </Disclosure>
         <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
         />
     </>
   )
--- src/pages/MyTask.jsx
+++ src/pages/MyTask.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TaskCard from "../components/TaskCard";
 import { useState } from "react";
 
@@ -8,6 +8,7 @@ import demoData from "../data/demo-task-card.json"
 
 export default function MyTask() {
 	const [taskData, setTaskData] = useState(demoData)
+	const [count, setCount] = useState(0)
 
 	let onDataChange = (name, status) => {
 		setTaskData(
@@ -18,9 +19,16 @@ export default function MyTask() {
 		)
 	}
 
+	useEffect(()=>{
+		setCount( taskData.filter( t => t.status == false).length)
+	},[taskData]);
+	
 	return (
 		<>
 			<div className="mx-auto max-w-9xl my py-4 sm:px-6 lg:px-8">
+				<div class="flex justify-center items-center h-12 bg-gray-200">
+					<p class="text-gray-700">請在一週內認識這{count}位新朋友!</p>
+				</div>
 				<div className=" h-fit py-6 px-2 rounded-lg border-4 border-dashed border-gray-200">
 					<div className="grid grid-cols-1 md:lg:xl:grid-cols-5 group bg-white shadow-xl shadow-neutral-100  ">
 						{taskData.map( (t, ind) => <TaskCard onDataChange={onDataChange} key={ind} ind={ind} name={t.name} dept={t.dept} status={t.status}></TaskCard>)}
